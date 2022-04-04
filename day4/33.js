759afced0540b628c94cd2ff33ce4f5c79383344
llama-ste <andong1212@gmail.com>
2022-04-04 18:08:23 +0900

    JS 100제 - 31~35번

---
+++ day4/33.js
@@ -0,0 +1,12 @@
+// 문제33 : 거꾸로 출력하기
+
+// 한 줄에 여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램을 작성하시오.
+
+function reverseNumList(nums) {
+  return nums
+    .split(" ")
+    .map((num) => Number(num))
+    .reverse();
+}
+
+console.log(...reverseNumList("1 2 3 4 5"));
